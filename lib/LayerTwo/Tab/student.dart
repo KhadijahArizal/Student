932a617d0f6c948ef5e9f12adcef935e5384f518
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-03 16:26:26 +0800

    student.dart

--- lib/LayerTwo/Tab/student.dart
+++ lib/LayerTwo/Tab/student.dart
@@ -1,103 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:student/LayerTwo/Tab/edit/studentForm.dart';
 
-// ignore: must_be_immutable
 class Student extends StatefulWidget {
-  late String name = '';
-  late String phone = '';
-  late String major = '';
-  late String ic = '';
-  late String address = '';
-
-  Student(
+  const Student(
       {Key? key,
-      required String name,
-      required String address,
-      required String ic,
-      required String phone,
-      required String major})
+      this.name,
+      this.contact,
+      this.address,
+      this.ic,
+      this.citizenship,
+      this.major})
       : super(key: key);
+
+  final String? name;
+  final String? contact;
+  final String? address;
+  final String? ic;
+  final String? citizenship;
+  final String? major;
 
   @override
   _studentState createState() => _studentState();
 }
 
 class _studentState extends State<Student> {
+  late TextEditingController name =
+      TextEditingController(text: widget.name ?? '-');
+  late TextEditingController contact =
+      TextEditingController(text: widget.contact ?? '-');
+  late TextEditingController address =
+      TextEditingController(text: widget.address ?? '-');
+  late TextEditingController ic = TextEditingController(text: widget.ic ?? '-');
+  late TextEditingController citizenship =
+      TextEditingController(text: widget.citizenship ?? '-');
+
   @override
-  Widget build(BuildContext context) {
-    final Map<String, String>? data =
-        ModalRoute.of(context)!.settings.arguments as Map<String, String>?;
-
-    if (data != null) {
-      setState(() {
-        widget.name = data['name'] ?? '';
-        widget.address = data['address'] ?? '';
-        widget.ic = data['ic'] ?? '';
-        widget.phone = data['phone'] ?? '';
-        widget.major = data['major'] ?? '';
-      });
-    }
-
-    return Center(
-      child: SingleChildScrollView(
-        child: Container(
-          color: Colors.white.withOpacity(0.1),
-          padding: const EdgeInsets.all(40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                _buildDetail('Name', widget.name),
-                _buildDetail('Phone No', widget.phone)
-              ]),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,children: [
-                  const Text('Email',
-                      style: TextStyle(fontSize: 13, color: Colors.black54),
-                      textAlign: TextAlign.start,),
-                  _email(email: 'admin@live.iium.edu.my')
-                ]),
-                _buildDetail('Major', widget.major)
-              ]),
-              const SizedBox(height: 50),
-              _buildDetail('IC/Passport No', widget.ic),
-              _buildDetail('Current Address', widget.address),
-              const SizedBox(height: 70),
-              
-              Container(
-                alignment: Alignment.bottomRight,
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Expanded(
-                      child: ElevatedButton.icon(
-                    onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const studentForm(),
-                      ),
-                    );
-                  },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(148, 112, 18, 1),
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    icon: const Icon(
-                      Icons.edit_rounded), // Icon data for elevated button
-                  label: const Text("Edit"),
-                  ))
-                ])),
-            ],
-          ),
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+    name = TextEditingController(text: widget.name ?? '-');
+    contact = TextEditingController(text: widget.contact ?? '-');
+    address = TextEditingController(text: widget.address ?? '-');
+    ic = TextEditingController(text: widget.ic ?? '-');
+    citizenship = TextEditingController(text: widget.citizenship ?? '-');
   }
 
   Widget _buildDetail(String label, String value) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 13, color: Colors.black54),
+        ),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 15, color: Colors.black87),
+        ),
+        const SizedBox(height: 50),
+      ],
+    );
+  }
+
+  Widget _buildDetail2(String label, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         Text(
           label,
@@ -122,4 +88,79 @@ class _studentState extends State<Student> {
           ],
         ),
       );
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.white.withOpacity(0.1),
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Name', name.text),
+              _buildDetail2('Matric No', 'generate from IAP form')
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Email', 'generate from login'),
+              _buildDetail2('Major', dropdownValueMajor)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Contact No', contact.text),
+              _buildDetail2('Current Address', address.text),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('IC/Passport No', ic.text),
+              _buildDetail2('Citizenship', citizenship.text)
+            ]),
+            const SizedBox(height: 70),
+            Container(
+                child: Row(children: [
+              Expanded(
+                  child: ElevatedButton.icon(
+                onPressed: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => studentForm(
+                        initialBr: dropDownValueBr,
+                        initialName: name.text,
+                        initialContact: contact.text,
+                        initialAddress: address.text,
+                        initialIc: ic.text,
+                        initialCitizenship: citizenship.text,
+                        initialMajor: dropdownValueMajor,
+                      ),
+                    ),
+                  );
+
+                  if (result != null) {
+                    setState(() {
+                      print('Result: $result');
+                      dropDownValueBr = result['br'] ?? '-';
+                      name.text = result['name'] ?? '-';
+                      contact.text = result['contact'] ?? '-';
+                      address.text = result['address'] ?? '-';
+                      ic.text = result['ic'] ?? '-';
+                      citizenship.text = result['citizenship'] ?? '-';
+                      dropdownValueMajor = result['major'] ?? '-';
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(148, 112, 18, 1),
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                icon: const Icon(
+                    Icons.edit_rounded), // Icon data for elevated button
+                label: const Text("Edit"),
+              ))
+            ])),
+          ],
+        ),
+      ),
+    );
+  }
 }
