9279f2a191d3c106f52d5fd20b2b99d90bb43776
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-03 16:25:12 +0800

    company.dart

--- lib/LayerTwo/Tab/company.dart
+++ lib/LayerTwo/Tab/company.dart
@@ -1,164 +1,177 @@
 import 'package:flutter/material.dart';
 import 'package:student/LayerTwo/Tab/edit/companyForm.dart';
 
-class Company extends StatelessWidget {
+class Company extends StatefulWidget {
+  const Company({
+    Key? key,
+    this.company,
+    this.letter,
+    this.zone,
+    this.monthlyA,
+    this.status,
+    this.address,
+    this.postcode,
+    this.duration,
+    this.start,
+    this.end,
+  }) : super(key: key);
+
+  final String? company,letter,zone,monthlyA,status,address,postcode,duration,start,end;
+
+  @override
+  _CompanyState createState() => _CompanyState();
+}
+
+class _CompanyState extends State<Company> {
+  late TextEditingController company =
+      TextEditingController(text: widget.company ?? '-');
+  late TextEditingController letter =
+      TextEditingController(text: widget.letter ?? '-');
+  late TextEditingController zone =
+      TextEditingController(text: widget.zone ?? '-');
+  late TextEditingController monthlyA =
+      TextEditingController(text: widget.monthlyA ?? '-');
+  late TextEditingController status =
+      TextEditingController(text: widget.status ?? '-');
+  late TextEditingController address =
+      TextEditingController(text: widget.address ?? '-');
+  late TextEditingController postcode =
+      TextEditingController(text: widget.postcode ?? '-');
+  late TextEditingController duration =
+      TextEditingController(text: widget.duration ?? '-');
+  late TextEditingController start =
+      TextEditingController(text: widget.status ?? '-');
+  late TextEditingController end =
+      TextEditingController(text: widget.status ?? '-');
+
+  @override
+  void initState() {
+    super.initState();
+    company = TextEditingController(text: widget.company ?? '-');
+    letter = TextEditingController(text: widget.letter ?? '-');
+    zone = TextEditingController(text: widget.zone ?? '-');
+    monthlyA = TextEditingController(text: widget.monthlyA ?? '-');
+    status = TextEditingController(text: widget.monthlyA ?? '-');
+    address = TextEditingController(text: widget.address ?? '-');
+    postcode = TextEditingController(text: widget.postcode ?? '-');
+    duration = TextEditingController(text: widget.duration ?? '-');
+    start = TextEditingController(text: widget.start ?? '-');
+    end = TextEditingController(text: widget.end ?? '-');
+  }
+
+  Widget _buildDetail(String label, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 13, color: Colors.black54),
+        ),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 15, color: Colors.black87),
+        ),
+        const SizedBox(height: 50),
+      ],
+    );
+  }
+
+  Widget _buildDetail2(String label, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(fontSize: 13, color: Colors.black54),
+        ),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 15, color: Colors.black87),
+        ),
+        const SizedBox(height: 50),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Container(
         color: Colors.white.withOpacity(0.1),
+        alignment: Alignment.bottomCenter,
         padding: const EdgeInsets.all(40),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Offer Letter',
-                  style: TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-                Text(
-                  'Zone',
-                  style: TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              'Monthly Allowance',
-              style: TextStyle(fontSize: 13, color: Colors.black54),
-            ),
-            const Text('RM', style: TextStyle(fontSize: 16)),
-            const SizedBox(height: 50),
-            const Text(
-              'Placement Verification Status',
-              style: TextStyle(fontSize: 13, color: Colors.black54),
-            ),
-            const SizedBox(height: 50),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Address',
-                  style: TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-                Text(
-                  'Postcode',
-                  style: TextStyle(fontSize: 13, color: Colors.black54),
-                ),
-              ],
-            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Offer Letter', letter.text),
+              _buildDetail2('Zone', dropDownValueZone)
+            ]),
+            _buildDetail('Industry', dropDownValueIndustry),
+            _buildDetail('Sector', dropDownValueSector),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Monthly Allowance', monthlyA.text),
+              _buildDetail2('Placement Status', status.text)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Address', address.text),
+              _buildDetail2('Postcode', postcode.text)
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              _buildDetail('Start Date', start.text),
+              _buildDetail2('End Date', end.text),
+            ]),
+            _buildDetail('Duration', duration.text),
             const SizedBox(height: 50),
             Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(10),
+                child: Row(children: [
+              Expanded(
+                  child: ElevatedButton.icon(
+                onPressed: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CompanyForm(
+                          initialCompany: company.text,
+                          initialIndustry: dropDownValueIndustry,
+                          initialSector: dropDownValueSector,
+                          initialZone: dropDownValueZone,
+                          initialAllowance: monthlyA.text,
+                          initialStatus: status.text,
+                          initialAddress: address.text,
+                          initialPostcode: postcode.text,
+                          initialDuration: duration.text,
+                          initialStart: start.text,
+                          initialEnd: end.text),
+                    ),
+                  );
+
+                  if (result != null) {
+                    setState(() {
+                      print('Result: $result');
+                      company.text = result['company'] ?? '-';
+                      dropDownValueIndustry = result['industry'] ?? '-';
+                      dropDownValueSector = result['sector'] ?? '-';
+                      dropDownValueZone = result['zone'] ?? '-';
+                      address.text = result['address'] ?? '-';
+                      postcode.text = result['postcode'] ?? '-';
+                      monthlyA.text = result['monthlyA'] ?? '-';
+                      start.text = result['start'] ?? '-';
+                      end.text = result['end'] ?? '-';
+                      duration.text = result['duration'] ?? '-';
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(148, 112, 18, 1),
+                  minimumSize: const Size.fromHeight(50),
                 ),
-                border: Border.all(
-                  width: 10,
-                  color: const Color.fromRGBO(148, 112, 18, 1),
-                ),
-                color: const Color.fromRGBO(148, 112, 18, 1),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      child: const Column(
-                        children: [
-                          Text(
-                            'Duration',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 13,
-                              fontFamily: 'Futura',
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Divider(
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      child: const Column(
-                        children: [
-                          Text(
-                            'Start Date',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 13,
-                              fontFamily: 'Futura',
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Divider(
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.white,
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      child: const Column(
-                        children: [
-                          Text(
-                            'End Date',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 13,
-                              fontFamily: 'Futura',
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Divider(
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 70),
-            Container(
-                alignment: Alignment.bottomRight,
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Expanded(
-                      child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const CompanyForm(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(148, 112, 18, 1),
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    icon: const Icon(
-                        Icons.edit_rounded), // Icon data for elevated button
-                    label: const Text("Edit"),
-                  ))
-                ])),
+                icon: const Icon(
+                    Icons.edit_rounded), // Icon data for elevated button
+                label: const Text("Edit"),
+              ))
+            ])),
           ],
         ),
       ),
