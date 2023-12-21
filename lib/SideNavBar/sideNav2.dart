89e3b28df92825944957698297007767e0cd0a97
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-22 06:21:55 +0800

    sideNav2.dart

--- lib/SideNavBar/sideNav2.dart
+++ lib/SideNavBar/sideNav2.dart
@@ -1,98 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:student/LayerTwo/faq.dart';
-import 'package:student/LayerTwo/lidv.dart';
-import 'package:student/LayerTwo/settings.dart';
+import 'package:student/LayerTwo/placements.dart';
+import 'package:student/LayerTwo/help.dart';
+import 'package:student/Service/auth_service.dart';
+import 'package:student/SignIn/SignIn.dart';
 import '../layerOne/coverLetter.dart';
 
 void main() => runApp(const MaterialApp(debugShowCheckedModeBanner: false));
 
 class sideNav2 extends StatelessWidget {
-   final String studentStatus; // Accept studentStatus as an optional parameter
+  final String studentStatus;
 
-  // Constructor that accepts studentStatus (optional)
   const sideNav2({required this.studentStatus});
 
   @override
   Widget build(BuildContext context) {
- bool isVisible = studentStatus == 'Active'; // Determine if LIDV menu should be visible
-
+    final AuthService authService = AuthService();
     return Drawer(
       backgroundColor: Colors.white,
       child: Material(
           color: Colors.white,
           child: ListView(children: <Widget>[
-                DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(148, 112, 18, 10),
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 146, 143, 10),
+              ),
+              child: Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.start, // Center the items horizontally
+                children: [
+                  Image.asset(
+                    'assets/iium.png', // Your image asset
+                    height: 50, // Adjust the height as needed
+                    width: 50, // Adjust the width as needed
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment
-                        .start, // Center the items horizontally
-                    children: [
-                      Image.asset(
-                        'assets/iium.png', // Your image asset
-                        height: 50, // Adjust the height as needed
-                        width: 50, // Adjust the width as needed
-                      ),
-                      const SizedBox(
-                          width: 10), // Add spacing between the image and text
-                      const Text(
-                        'i-KICT',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.w900,
-                          fontFamily: 'Futura',
-                        ),
-                      ),
-                    ],
+                  const SizedBox(
+                      width: 10), // Add spacing between the image and text
+                  const Text(
+                    'i-KICT',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w900,
+                      fontFamily: 'Futura',
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                buildMenuItem(
-                    text: 'Cover Letter',
-                    icon: Icons.email_rounded,
-                    onClicked: (() => selectedItem(context, 0))),
-                const SizedBox(
-                  height: 10,
-                ),
-                Visibility(
-               visible: isVisible, // Pass visibility condition to buildMenuItem
-              child: buildMenuItem(
-                    text: 'LIDV',
-                    icon: Icons.home_filled,
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            buildMenuItem(
+                text: 'Cover Letter',
+                icon: Icons.email_rounded,
+                onClicked: (() => selectedItem(context, 0))),
+            const SizedBox(
+              height: 10,
+            ),
+            Visibility(
+                child: buildMenuItem(
+                    text: 'Placements',
+                    icon: Icons.place_rounded,
                     onClicked: (() => selectedItem(context, 1)))),
-                const SizedBox(
-                  height: 10,
-                ),
-                buildMenuItem(
-                    text: 'FAQ',
-                    icon: Icons.question_answer_rounded,
-                    onClicked: (() =>
-                        selectedItem(context, 2))), //_visible = false
-                const SizedBox(
-                  height: 10,
-                ),
-                buildMenuItem(
-                    text: 'Settings',
-                    icon: Icons.settings,
-                    onClicked: (() => selectedItem(context, 3))),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Divider(
-                  color: Colors.black54,
-                ),
-                buildMenuItem(
-                  text: 'Logout',
-                  icon: Icons.exit_to_app_rounded,
-                  onClicked: () => SystemNavigator.pop(),
-                ),
-              ])),
+            const SizedBox(
+              height: 10,
+            ),
+            buildMenuItem(
+                text: 'FAQ',
+                icon: Icons.question_answer_rounded,
+                onClicked: (() => selectedItem(context, 2))), //_visible = false
+            const SizedBox(
+              height: 10,
+            ),
+            buildMenuItem(
+                text: 'Help',
+                icon: Icons.help_center_rounded,
+                onClicked: (() => selectedItem(context, 3))),
+            const SizedBox(
+              height: 15,
+            ),
+            const Divider(
+              color: Colors.black54,
+            ),
+            buildMenuItem(
+              text: 'Logout',
+              icon: Icons.exit_to_app_rounded,
+              onClicked: () async {
+                try {
+                  await authService.handleSignOut();
+                  // Navigate to the sign-in page or any other desired page after logout
+                  Navigator.of(context).pop(); // Close the drawer
+                  // Navigate to the sign-in page or any other desired page after logout
+                  // For example, you can use Navigator to go to the sign-in page:
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) =>
+                        SignIn(), // Replace with your sign-in page
+                  ));
+                  // Display the success message
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: const Text('YEAYY! Logout'),
+                      duration: const Duration(seconds: 2),
+                    ),
+                  );
+                } catch (e) {
+                  print("Error during logout: $e");
+                }
+              },
+            ),
+          ])),
     );
   }
 
@@ -103,7 +122,7 @@ class sideNav2 extends StatelessWidget {
     return ListTile(
         leading: Icon(
           icon,
-          color: const Color.fromRGBO(148, 112, 18, 10),
+          color: const Color.fromRGBO(0, 168, 80, 10),
         ),
         title: Text(
           text,
@@ -123,7 +142,10 @@ class sideNav2 extends StatelessWidget {
         break;
       case 1:
         Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => const LIDV(title: 'LIDV'),
+          builder: (context) => const Placements(
+            title: 'Placements',
+            companyName: '',
+          ),
         ));
         break;
       case 2:
@@ -133,7 +155,7 @@ class sideNav2 extends StatelessWidget {
         break;
       case 3:
         Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => const Settings(title: 'Settings'),
+          builder: (context) => const Help(title: 'Help'),
         ));
         break;
     }
