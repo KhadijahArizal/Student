5d8427a3fde70c44ab9550b8a474cd9653009093
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-27 15:27:07 +0800

    faq.dart

--- lib/LayerTwo/faq.dart
+++ lib/LayerTwo/faq.dart
@@ -1,5 +1,7 @@
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 class FAQ extends StatefulWidget {
   const FAQ({Key? key, required String title}) : super(key: key);
 
@@ -8,41 +10,66 @@ class FAQ extends StatefulWidget {
 }
 
 class _FAQState extends State<FAQ> {
+  late PDFDocument _coverLetterPdfDocument;
+  late PDFDocument _placementVerificationPdfDocument;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadPDFs();
+  }
+
+  void loadPDFs() async {
+    try {
+      _coverLetterPdfDocument =
+          await PDFDocument.fromAsset('assets/Help/iap_cover_letter.pdf');
+      _placementVerificationPdfDocument = await PDFDocument.fromAsset(
+          'assets/Help/iap_placement_verification.pdf');
+
+      setState(() {
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error loading PDFs: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
-        appBar: AppBar(
+      backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
           leading: IconButton(
-          icon: Icon(
-            size: 25,
-            Icons.arrow_back_ios_new_rounded,
-            color: Colors.black87.withOpacity(0.7),// Use the specified color
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-            title: const Text(
-              'FAQ',
-              style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: 'Futura'),
+            icon: const Icon(
+              Icons.arrow_back_ios_new_rounded,
+              size: 25,
+              color: Color.fromRGBO(0, 146, 143, 10), // Use the specified color
             ),
-            backgroundColor: Colors.white,
-            elevation: 0,
-            systemOverlayStyle: SystemUiOverlayStyle.dark,
-            iconTheme: const IconThemeData(
-              color: Color.fromRGBO(148, 112, 18, 1),
-              size: 30,
-            )),
-        body: SafeArea(
-            child: Container(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: const Text(
+            'FAQ',
+            style: TextStyle(
+                color: Colors.black87,
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'Futura'),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          iconTheme: const IconThemeData(
+            color: Color.fromRGBO(0, 146, 143, 10),
+            size: 30,
+          )),
+      body: SafeArea(
+        child: Container(
           padding: const EdgeInsets.all(20),
           width: double.infinity,
-                height: double.infinity,
+          height: double.infinity,
           decoration: BoxDecoration(
             color: Colors.white,
             image: DecorationImage(
@@ -52,182 +79,61 @@ class _FAQState extends State<FAQ> {
                   Colors.white30.withOpacity(0.2), BlendMode.dstATop),
             ),
           ),
-          child: Column(children: [
-              ExpansionTile(
-                  title: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 5),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [
-                            BoxShadow(color: Color.fromRGBO(148, 112, 18, 1))
-                          ],
-                          border: Border.all(
-                              color: const Color.fromRGBO(148, 112, 18, 1),
-                              width: 7)),
-                      child: const Text(
-                        'Industrial Attachment Programme Cover Letter',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Futura'),
-                            textAlign: TextAlign.start,
-                      )),
-                  children: <Widget>[
-                     Padding( 
-                      padding: const EdgeInsets.all(10),
-                      child:RichText(
-                        text: TextSpan(
-                            style: const TextStyle(
-                                color: Colors.black87,
-                                fontSize: 15,
-                                fontFamily: 'Futura'),
-                            children: [
-                          const TextSpan(
-                            text: 'IAP ',
-                          ),
-                          const TextSpan(
-                              text: 'Start Date ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'must be within the '),
-                          const TextSpan(
-                              text: 'FIRST 3 WEEKS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(
-                              text:
-                                  'of every semester. Any IAP starts before that will '),
-                          TextSpan(
-                              text: 'NOT ',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red[800])),
-                          const TextSpan(text: 'count as '),
-                          const TextSpan(
-                              text: '6 MONTHS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'duration.'),
-                        ]))),
-                  ]),
-
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  // Title for 'IAP Cover Letter'
                   ExpansionTile(
-                  title: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 5),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [
-                            BoxShadow(color: Color.fromRGBO(148, 112, 18, 1))
-                          ],
-                          border: Border.all(
-                              color: const Color.fromRGBO(148, 112, 18, 1),
-                              width: 7)),
-                      child: const Text(
-                        'Industrial Attachment Programme List of Company',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Futura'),
-                            textAlign: TextAlign.start,
-                      )),
-                  children: <Widget>[
-                     Padding( 
-                      padding: const EdgeInsets.all(10),
-                      child:RichText(
-                        text: TextSpan(
-                            style: const TextStyle(
-                                color: Colors.black87,
-                                fontSize: 15,
-                                fontFamily: 'Futura'),
-                            children: [
-                          const TextSpan(
-                            text: 'IAP ',
-                          ),
-                          const TextSpan(
-                              text: 'Start Date ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'must be within the '),
-                          const TextSpan(
-                              text: 'FIRST 3 WEEKS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(
-                              text:
-                                  'of every semester. Any IAP starts before that will '),
-                          TextSpan(
-                              text: 'NOT ',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red[800])),
-                          const TextSpan(text: 'count as '),
-                          const TextSpan(
-                              text: '6 MONTHS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'duration.'),
-                        ]))),
-                  ]),
+                    title: const Text(
+                      'IAP Cover Letter',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    children: [
+                      // PDF viewer for 'IAP Cover Letter'
+                      SizedBox(
+                        height: 300, // Adjust the height as needed
+                        child: _isLoading
+                            ? const Center(child: CircularProgressIndicator())
+                            : PDFViewer(
+                                document: _coverLetterPdfDocument,
+                              ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10), // Add some spacing
 
+                  // Title for 'IAP Placement Verification'
                   ExpansionTile(
-                  title: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 5, vertical: 5),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: const [
-                            BoxShadow(color: Color.fromRGBO(148, 112, 18, 1))
-                          ],
-                          border: Border.all(
-                              color: const Color.fromRGBO(148, 112, 18, 1),
-                              width: 7)),
-                      child: const Text(
-                        'Placements Verifications',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Futura'),
-                            textAlign: TextAlign.start,
-                      )),
-                  children: <Widget>[
-                     Padding( 
-                      padding: const EdgeInsets.all(10),
-                      child:RichText(
-                        text: TextSpan(
-                            style: const TextStyle(
-                                color: Colors.black87,
-                                fontSize: 15,
-                                fontFamily: 'Futura'),
-                            children: [
-                          const TextSpan(
-                            text: 'IAP ',
-                          ),
-                          const TextSpan(
-                              text: 'Start Date ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'must be within the '),
-                          const TextSpan(
-                              text: 'FIRST 3 WEEKS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(
-                              text:
-                                  'of every semester. Any IAP starts before that will '),
-                          TextSpan(
-                              text: 'NOT ',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red[800])),
-                          const TextSpan(text: 'count as '),
-                          const TextSpan(
-                              text: '6 MONTHS ',
-                              style: TextStyle(fontWeight: FontWeight.bold)),
-                          const TextSpan(text: 'duration.'),
-                        ]))),
-                  ]),
-            
-          ]),
-        )));
+                    title: const Text(
+                      'IAP Placement Verification',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    children: [
+                      // PDF viewer for 'IAP Placement Verification'
+                      SizedBox(
+                        height: 300, // Adjust the height as needed
+                        child: _isLoading
+                            ? const Center(child: CircularProgressIndicator())
+                            : PDFViewer(
+                                document: _placementVerificationPdfDocument,
+                              ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
