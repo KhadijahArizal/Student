d21dcd73ad00e8ffbd0b824846f6caaf58be27f0
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2023-12-27 15:27:25 +0800

    placements.dart

--- lib/LayerTwo/placements.dart
+++ lib/LayerTwo/placements.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:student/BottomNavBar/bottomMenu.dart';
-import 'package:student/SideNavBar/sideNav2.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:provider/provider.dart';
+import 'package:student/LayerTwo/Tab/data.dart';
+import 'package:student/LayerTwo/Tab/examiner.dart';
 import 'package:student/LayerTwo/Tab/company.dart';
 import 'package:student/LayerTwo/Tab/supervisor.dart';
 import 'Detect Status/statusManagament.dart';
@@ -15,27 +19,36 @@ class Placements extends StatefulWidget {
     Key? key,
     required String title,
     required this.companyName,
-    required this.companyAddress,
-    required this.companyPostcode,
-    required this.monthlyAllowance,
   }) : super(key: key);
 
-  final String companyName, companyAddress, companyPostcode, monthlyAllowance;
+  final String companyName;
 
   @override
   // ignore: library_private_types_in_public_api
   _PlacementsState createState() => _PlacementsState();
+
+  void onOfferLetterSelected(String? offerLetter) {
+    if (offerLetter != null) {
+      openOfferLetter(offerLetter);
+    }
+  }
+
+  void openOfferLetter(String offerLetter) async {
+    try {
+      await OpenFilex.open(offerLetter);
+    } catch (e) {
+      print('Error opening offer letter: $e');
+      // Handle the error, e.g., show an error message to the user
+    }
+  }
 }
 
 class _PlacementsState extends State<Placements>
     with SingleTickerProviderStateMixin {
-  final TextEditingController _supervisor = TextEditingController();
-  final TextEditingController _email = TextEditingController();
-  final TextEditingController _contact = TextEditingController();
-  late String _companyName;
   final StatusManagement statusManager = StatusManagement();
   late TabController tabController;
   late StatusManagement _statusManagement;
+  var examinerInstance;
 
   Widget _cname({required String cname}) => Container(
         alignment: Alignment.center,
@@ -53,42 +66,42 @@ class _PlacementsState extends State<Placements>
   @override
   void initState() {
     super.initState();
-    _companyName = widget.companyName;
     _statusManagement = StatusManagement();
-    tabController = TabController(length: 2, vsync: this);
+    tabController = TabController(length: 3, vsync: this);
+    tabController.addListener(_handleTabChangeWithoutArgument);
   }
 
   @override
   void dispose() {
     _statusManagement.dispose();
+    tabController.removeListener(_handleTabChangeWithoutArgument);
     tabController.dispose();
     super.dispose();
   }
 
-  int _currentIndex = 4;
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-      if (index == 0) {
-        Navigator.pushNamed(context, '/summary');
-      } else if (index == 1 && _statusManagement.studentStatus == 'Active') {
-        Navigator.pushNamed(context, '/monthly_report');
-      } else if (index == 2 && _statusManagement.studentStatus == 'Active') {
-        Navigator.pushNamed(context, '/final_report');
-      } else if (index == 3) {
-        Navigator.pushNamed(context, '/details');
-      } else if (index == 4) {
-        Navigator.pushNamed(context, '/placements');
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    var studentData = Provider.of<Data>(context);
+    examinerInstance = Examiner(
+      examiner: studentData.exname.text,
+      email: studentData.exemail.text,
+      contact: studentData.excontact.text,
+      handleTabChange: _handleTabChangeWithoutArgument, studentData: studentData,
+    );
     return Scaffold(
       backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+                size: 25,
+                color:
+                    Color.fromRGBO(0, 146, 143, 10), // Use the specified color
+              ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/summary');
+          },
+        ),
         title: const Text(
           'Placements',
           style: TextStyle(
@@ -101,22 +114,10 @@ class _PlacementsState extends State<Placements>
         elevation: 0,
         systemOverlayStyle: SystemUiOverlayStyle.dark,
         iconTheme: const IconThemeData(
-          color: Color.fromRGBO(148, 112, 18, 1),
+          color: Color.fromRGBO(0, 146, 143, 10),
           size: 30,
         ),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.sort,
-                  color: Color.fromRGBO(148, 112, 18, 1), size: 30),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
-        ),
       ),
-      drawer: sideNav2(studentStatus: statusManager.studentStatus),
       body: SafeArea(
           child: Container(
               padding: const EdgeInsets.all(10),
@@ -136,117 +137,121 @@ class _PlacementsState extends State<Placements>
                 Container(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 10, vertical: 10),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          SingleChildScrollView(
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: SizedBox(
-                                height: MediaQuery.of(context).size.height,
-                                child: Column(
-                                    mainAxisSize: MainAxisSize.max,
-                                    children: [
-                                      Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                              Radius.circular(10),
-                                            ),
-                                            border: Border.all(
-                                                width: 10,
-                                                color: const Color.fromRGBO(
-                                                    148, 112, 18, 1)),
-                                            color: const Color.fromRGBO(
-                                                148, 112, 18, 1)),
-                                        child: Column(children: [
-                                          Container(
-                                            padding: const EdgeInsets.all(10),
-                                            child: Column(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: [
-                                                  const Text(
-                                                    'Company Name',
-                                                    style: TextStyle(
-                                                        color: Colors.white70,
-                                                        fontSize: 13,
-                                                        fontFamily: 'Futura',
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                                  ),
-                                                  const SizedBox(height: 10),
-                                                  _cname(
-                                                      cname: _companyName), //COBA-COBA
-                                                ]),
-                                          ),
-                                          const SizedBox(height: 5),
-                                          DefaultTabController(
-                                            length: 2,
-                                            child: TabBar(
-                                              unselectedLabelColor:
-                                                  Colors.white,
-                                              labelColor: const Color.fromRGBO(
-                                                  148, 112, 18, 1),
-                                              indicatorColor: Colors.white,
-                                              indicatorWeight: 0.0,
-                                              indicatorSize:
-                                                  TabBarIndicatorSize.tab,
-                                              indicator: BoxDecoration(
-                                                borderRadius:
-                                                    BorderRadius.circular(5),
-                                                color: Colors.white,
+                    child: Consumer<Data>(builder: (context, studentData, child) {
+                      return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            SingleChildScrollView(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: SizedBox(
+                                  height: MediaQuery.of(context).size.height,
+                                  child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Container(
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  const BorderRadius.all(
+                                                Radius.circular(10),
                                               ),
-                                              controller: tabController,
-                                              tabs: const [
-                                                Tab(
-                                                    icon: Icon(Icons.domain),
-                                                    text: ' Company'),
-                                                Tab(
-                                                  icon: Icon(Icons.person),
-                                                  text: 'Supervisor',
-                                                ),
-                                              ],
+                                              border: Border.all(
+                                                  width: 10,
+                                                  color: const Color.fromRGBO(
+                                                      0, 146, 143, 10)),
+                                              color: const Color.fromRGBO(
+                                                  0, 146, 143, 10)),
+                                          child: Column(children: [
+                                            Container(
+                                              padding: const EdgeInsets.all(10),
+                                              child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    const Text(
+                                                      'Company Name',
+                                                      style: TextStyle(
+                                                          color: Colors.white70,
+                                                          fontSize: 13,
+                                                          fontFamily: 'Futura',
+                                                          fontWeight:
+                                                              FontWeight.bold),
+                                                    ),
+                                                    const SizedBox(height: 10),
+                                                    _cname(
+                                                        cname: studentData
+                                                            .company.text),
+                                                  ]),
                                             ),
-                                          ),
-                                        ]),
-                                      ),
-                                      Expanded(
-                                        child: TabBarView(
-                                          controller: tabController,
-                                          children: [
-                                            const Company(),
-                                            Supervisor(
-                                              onSupervisorChanged:
-                                                  (String supervisor,
-                                                      String email) {
-                                                supervisor;
-                                                email;
-                                              },
-                                            )
-                                          ],
+                                            const SizedBox(height: 5),
+                                            DefaultTabController(
+                                              length: 3,
+                                              child: TabBar(
+                                                unselectedLabelColor:
+                                                    Colors.white,
+                                                labelColor:
+                                                    const Color.fromRGBO(
+                                                        0, 146, 143, 10),
+                                                indicatorColor: Colors.white,
+                                                indicatorWeight: 0.0,
+                                                indicatorSize:
+                                                    TabBarIndicatorSize.tab,
+                                                indicator: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(5),
+                                                  color: Colors.white,
+                                                ),
+                                                controller: tabController,
+                                                tabs: const [
+                                                  Tab(
+                                                      icon: Icon(Icons.domain),
+                                                      text: ' Company'),
+                                                  Tab(
+                                                    icon: Icon(Icons.person),
+                                                    text: 'Supervisor',
+                                                  ),
+                                                  Tab(
+                                                    icon: Icon(Icons.person),
+                                                    text: 'Examiner',
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ]),
                                         ),
-                                      )
-                                    ]),
+                                        Expanded(
+                                          child: TabBarView(
+                                            controller: tabController,
+                                            children: [
+                                              Company(onOfferLetterSelected: widget.onOfferLetterSelected),
+                                              const Supervisor(),
+                                              Examiner(
+                                                handleTabChange: () {}, studentData: studentData,
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ]),
+                                ),
                               ),
                             ),
-                          ),
-                        ])),
+                          ]);
+                    })),
               ])))),
-      bottomNavigationBar: BottomMenu(
-        currentIndex: _currentIndex,
-        onTap: onTabTapped,
-        routeNames: const {
-          'Summary': '/summary',
-          'Monthly Report': '/monthly_report',
-          'Final Report': '/final_report',
-          'Details': '/details',
-          'Placements': '/placements',
-        },
-        studentStatus: _statusManagement.studentStatus,
-      ),
     );
   }
+
+  void _handleTabChange(Data studentData) {
+    if (tabController.index == 2) {
+      examinerInstance.updateDatabase(studentData: studentData);
+    }
+  }
+
+  void _handleTabChangeWithoutArgument() {
+  _handleTabChange(Provider.of<Data>(context));
+}
+
+ 
 }
