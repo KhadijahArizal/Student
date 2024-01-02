9da04ce2fac0587116bc221e5e6cc663b5b3c2b3
Zahra Fathanah <97120878+kangmanis@users.noreply.github.com>
2024-01-03 03:02:17 +0800

    Update auth_service.dart

--- lib/Service/auth_service.dart
+++ lib/Service/auth_service.dart
@@ -8,29 +8,37 @@ class AuthService {
           '753383357173-e2pqr9f4bnfuv1p94oqekvu51u3o1qbu.apps.googleusercontent.com');
 
 //google sign in
-  Future<void> handleSignin() async {
-    try {
-      GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      if (googleUser != null) {
-        GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-        AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken,
-          idToken: googleAuth.idToken,
-        );
+  // AuthService
+Future<User?> handleSignin() async {
+  try {
+    GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-        UserCredential authResult =
-            await _auth.signInWithCredential(credential);
-        User? user = authResult.user;
+      UserCredential authResult = await _auth.signInWithCredential(credential);
+      User? user = authResult.user;
 
-        // Set the photo URL if available
-        if (googleUser.photoUrl != null) {
-          await user?.updatePhotoURL(googleUser.photoUrl!);
-        }
+      // Set the photo URL if available
+      if (googleUser.photoUrl != null) {
+        await user?.updatePhotoURL(googleUser.photoUrl!);
       }
-    } catch (e) {
-      print("Error signing in with Google $e");
+
+      return user;
+    } else {
+      // Handle the case where the user cancels the sign-in process
+      print("User canceled sign-in");
+      return null;
     }
+  } catch (e) {
+    print("Error signing in with Google $e");
+    return null;
   }
+}
+
 
   // Google sign out
   Future<void> handleSignOut() async {
@@ -44,5 +52,19 @@ class AuthService {
 
   // Get authenticated user
   User? get currentUser => _auth.currentUser;
+
+  //ifpopupclosed
+  Future<bool> isSignInResultValid(User? user) async {
+  if (user != null) {
+    // Check if the user has the required attributes or roles
+    if (user.displayName != null && user.email != null) {
+      // Additional validation logic can be added here
+      // For example, check if the user is an administrator or has certain roles
+      // Replace this with your actual validation criteria
+      return true;
+    }
+  }
+  return false;
+}
 
 }
